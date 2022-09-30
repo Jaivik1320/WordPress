e0e23e63f7af49705395376b44234decac5abe78
Jaivik Parekh <76834763+Jaivik1320@users.noreply.github.com>
2022-09-30 10:12:49 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,39 +1,45 @@
-<?php get_header() ?>
-<!-- Main -->
-<section id="main">
-    <div class="container">
-        <div class="row">
-            <div class="col-8 col-12-medium">
-                <?php
-                    if(have_posts()) {
-                        while(have_posts()) {
-                            the_post(); ?>
-                            <div class="col-6 col-12-small">
-                                <section class="box">
-                                    <a href="<?php the_permalink() ?>" class="image featured">
-                                        <?php the_post_thumbnail('home-featured') ?>
-                                    </a>
-                                    <header>
-                                        <h3><?php the_title() ?></h3>
-                                        <p>Posted on <?php the_date() ?> at <?php the_time() ?></p>
-                                    </header>
-                                    <?php the_excerpt() ?>
-                                    <footer>
-                                        <ul class="actions">
-                                            <li><a href="<?php the_permalink() ?>" class="button icon solid fa-file-alt">Continue Reading</a></li>
-                                            <li><a href="<?php comments_link() ?>" class="button alt icon solid fa-comment"><?php echo get_comments_number( ) ?> comments</a></li>
-                                        </ul>
-                                    </footer>
-                                </section>
-                            </div>
-                    <?php }
-                    }
-                ?>
-                
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
 
-            </div>
-            <?php get_sidebar() ?>
-        </div>
-    </div>
-</section>
-<?php get_footer() ?>
+get_header(); ?>
+
+<?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
+	<header class="page-header alignwide">
+		<h1 class="page-title"><?php single_post_title(); ?></h1>
+	</header><!-- .page-header -->
+<?php endif; ?>
+
+<?php
+if ( have_posts() ) {
+
+	// Load posts loop.
+	while ( have_posts() ) {
+		the_post();
+
+		get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) );
+	}
+
+	// Previous/next page navigation.
+	twenty_twenty_one_the_posts_navigation();
+
+} else {
+
+	// If no content, include the "No posts found" template.
+	get_template_part( 'template-parts/content/content-none' );
+
+}
+
+get_footer();
