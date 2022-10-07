1260c3e590dd59c1c0dad202c4dba264e00cb5d1
Jaivik Parekh <76834763+Jaivik1320@users.noreply.github.com>
2022-10-07 11:12:22 +0530

    Add files via upload

--- front-page.php
+++ front-page.php
@@ -1,98 +1,60 @@
-<?php get_header(); ?>
+  <?php get_header(); ?>
+        
+        <div class="main-content">
+   	    	<img src="<?php echo get_template_directory_uri(); ?>/images/top-banner.jpg"/>
+        </div><!--ends main content-->
+        
+        
+        <div id="main-page-header-bg">
+        	<div class="main-content">
+            	<h1>Welcome to <span style="color:#2c8aaa;">White Tiger Express</span></h1>
+            </div><!--ends main-content-->
+        </div><!--ends main-page-header-bg-->
+        
+        
+        <div id="small-imgs-container">
+        <div class="main-content">
+        	
+            <div id="left-small-img">
+            	<img src="<?php echo get_template_directory_uri(); ?>/images/home-small-img-1.png" /> 
+                <h3>Transportation Services</h3>
+                <p>Trust White Tiger Express to be your logistics provider. Our truckload services are custom designed to meet the unique requirements of our customers. Whether you are a small and attention you would expect of a family owned and operated business. White Tiger Express specializes in full truckload shipments to traffic lanes along the West Coast.. <a href="#">View More</a></p>
+            </div><!--ends left small img-->
+            
+            <div id="right-small-img">
+            	<img src="<?php echo get_template_directory_uri(); ?>/images/home-small-img-2.png" /> 
+                <h3>Emplyement</h3>
+                <p>White Tiger Express focuses on safe, reliable and professional service to our customers. As our company grows, we may have openings for experienced, trained drivers who also value safety, reliability and providing superior service to White Tiger Express customers. <a href="#">View More</a></p>
+            </div><!--ends right-small-img--> 
+            
+          </div><!--ends main-content-->       
+</div><!--ends small imgs container-->
+         
+         
+         <div class="clear"></div>
+         <div class="main-content">
+         
+         <div id="bottom-description-header">
+   	     	<img src="<?php echo get_template_directory_uri(); ?>/images/small-vehicle.jpg" /> 
+            <h4>Premium, Cost- Effective and Professional Logistics Provider</h4>
+         </div><!--ends bottom-description-->
+         
+         <div class="clear"></div>
+         <div id="bottom-description">
+         	<img src="<?php echo get_template_directory_uri(); ?>/images/bottom-big-image.jpg" />
+            <p>White Tiger Express is the premium transportation provider you’re looking for if you want competitive rates, reduce give-back loads, reduce claims, and drive your distribution profits up without sacrificing quality. White Tiger Express offers competitive, 24-hours a day/7-days a week accessibility, cost-effective pricing with a commitment to safety, reliability and on-time deliveries. Focusing on customer service and client satisfaction are our primary goals. <br />
+<br />
 
-<?php dynamic_sidebar( 'home-banner' ); ?>
-<!-- Intro -->
-<?php dynamic_sidebar( 'home-services' ) ?>
+Our experienced professionals work to find the best transportation solutions to meet our customer’s unique needs. We have our own agents who work with our customers to find the best solution to their logistic needs. Whether you’re a small company or large corporation, <br />
+<br />
 
-			
-<!-- Main -->
-	<section id="main">
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
+White Tiger Express can handle your individual logistics needs on the West Coast (California, Oregon and Washington) as well as Arizona and Utah.  If you’re looking for a premium, cost-effective and professional solution to your transportation requirements, contact us.</p>
+         </div>
+         
+         </div><!--ends main-content-->
+         
+         
+        <div class="clear"></div>
 
-				<!-- Portfolio -->
-				<section>
-					<header class="major">
-						<h2>My Portfolio</h2>
-					</header>
-					<div class="row">
+        <?php get_footer(); ?>
 
-						<?php
-							$portfolio_args = array(
-								'post_type'	=> 'portfolio',
-								'posts_per_page'	=> 6
-							);
-							$portfolio_posts = new WP_Query($portfolio_args);
-							while($portfolio_posts->have_posts()) {
-								$portfolio_posts->the_post();
-						?>
-						<div class="col-4 col-6-medium col-12-small">
-							<section class="box">
-								<a href="<?php the_permalink() ?>" class="image featured">
-									<?php the_post_thumbnail('portfolio-thumb') ?>
-								</a>
-								<header>
-									<h3><?php the_title() ?></h3>
-								</header>
-								<?php the_excerpt() ?>
-								<footer>
-									<ul class="actions">
-										<li><a href="<?php the_permalink() ?>" class="button alt">Find out more</a></li>
-									</ul>
-								</footer>
-							</section>
-						</div>
-						<?php } ?>
-						<?php wp_reset_postdata() ?>
-						
-					</div>
-				</section>
-
-			</div>
-			<div class="col-12">
-
-				<!-- Blog -->
-				<section>
-					<header class="major">
-						<h2>The Blog</h2>
-					</header>
-					<div class="row">
-						<?php
-							$blog_args = array(
-								'post_type'	=> 'post',
-								'posts_per_page'	=> 2
-							);
-							$blog_posts = new WP_Query($blog_args);
-							while($blog_posts->have_posts()) {
-								$blog_posts->the_post();
-						?>
-						<div class="col-6 col-12-small">
-							<section class="box">
-								<a href="<?php the_permalink() ?>" class="image featured">
-									<?php the_post_thumbnail('home-featured') ?>
-								</a>
-								<header>
-									<h3><?php the_title() ?></h3>
-									<p>Posted on <?php the_date() ?> at <?php the_time() ?></p>
-								</header>
-								<?php the_excerpt() ?>
-								<footer>
-									<ul class="actions">
-										<li><a href="<?php the_permalink() ?>" class="button icon solid fa-file-alt">Continue Reading</a></li>
-										<li><a href="<?php comments_link() ?>" class="button alt icon solid fa-comment"><?php echo get_comments_number( ) ?> comments</a></li>
-									</ul>
-								</footer>
-							</section>
-						</div>
-						<?php } ?>
-						
-					</div>
-				</section>
-
-			</div>
-		</div>
-	</div>
-</section>
-
-<?php get_footer(); ?>
