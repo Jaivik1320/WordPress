41c8e6f62d106eccf050acd23e4acf6de63d8810
Jaivik Parekh <76834763+Jaivik1320@users.noreply.github.com>
2022-10-12 11:38:26 +0530

    Add files via upload

--- single.php
+++ single.php
@@ -1,50 +1,60 @@
+<?php get_header(); 
+
+
+?>
+ 
+ 
 <?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
- */
+// Start the Loop.
+if ( have_posts() ) :
+    while ( have_posts() ) : the_post();
+        /* * See if the current post is in category 3.
+          * If it is, the div is given the CSS class "post-category-three".
+          * Otherwise, the div is given the CSS class "post".
+        */
+        if ( in_category( 3 ) ) : ?>
+        <div class="post-category-three">
+        <?php else : ?>
+        <div class="post">
+        <?php endif; 
 
-get_header();
+            // Display the post's title.
+            the_title( '<h2>', ';</h2>' ); 
 
-/* Start the Loop */
-while ( have_posts() ) :
-	the_post();
+            // Display a link to other posts by this posts author.
+            printf( __( 'Posted by %s', 'textdomain' ), get_the_author_posts_link() );
 
-	get_template_part( 'template-parts/content/content-single' );
+            // Display the post's content in a div.
+            ?>
+            <div class="entry">
+                <?php the_content() ?>
+             </div>
+             <!--  ; ?> -->
+             <a href="<?php echo get_permalink(); ?>">click here</a>
 
-	if ( is_attachment() ) {
-		// Parent post navigation.
-		the_post_navigation(
-			array(
-				/* translators: %s: Parent post link. */
-				'prev_text' => sprintf( __( '<span class="meta-nav">Published in</span><span class="post-title">%s</span>', 'twentytwentyone' ), '%title' ),
-			)
-		);
-	}
+            <?php
+            // Display a comma separated list of the post's categories.
+            _e( 'Posted in ', 'textdomain' ); the_category( ', ' ); 
 
-	// If comments are open or there is at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	}
+        // closes the first div box with the class of "post" or "post-cat-three"
+       ?>
+       </div>
 
-	// Previous/next post navigation.
-	$twentytwentyone_next = is_rtl() ? twenty_twenty_one_get_icon_svg( 'ui', 'arrow_left' ) : twenty_twenty_one_get_icon_svg( 'ui', 'arrow_right' );
-	$twentytwentyone_prev = is_rtl() ? twenty_twenty_one_get_icon_svg( 'ui', 'arrow_right' ) : twenty_twenty_one_get_icon_svg( 'ui', 'arrow_left' );
+    <?php
+    // Stop the Loop, but allow for a "if not posts" situation
+    endwhile; 
 
-	$twentytwentyone_next_label     = esc_html__( 'Next post', 'twentytwentyone' );
-	$twentytwentyone_previous_label = esc_html__( 'Previous post', 'twentytwentyone' );
+else :
+    /*
+      * The very first "if" tested to see if there were any posts to
+      * display. This "else" part tells what do if there weren't any.
+     */
+     _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+   
+// Completely stop the Loop.
+ endif;
+?>
+<!-- permalink -->
 
-	the_post_navigation(
-		array(
-			'next_text' => '<p class="meta-nav">' . $twentytwentyone_next_label . $twentytwentyone_next . '</p><p class="post-title">%title</p>',
-			'prev_text' => '<p class="meta-nav">' . $twentytwentyone_prev . $twentytwentyone_previous_label . '</p><p class="post-title">%title</p>',
-		)
-	);
-endwhile; // End of the loop.
+<?php get_footer(); ?>
 
-get_footer();
