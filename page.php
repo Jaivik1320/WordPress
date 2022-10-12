4e601ec143612be82302c0c8a6114b1b32aa4ce6
Jaivik Parekh <76834763+Jaivik1320@users.noreply.github.com>
2022-10-12 11:38:15 +0530

    Add files via upload

--- page.php
+++ page.php
@@ -1,25 +1,6 @@
-<?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
- */
+<?php get_header(); ?>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
 
-get_header();
-
-/* Start the Loop */
-while ( have_posts() ) :
-	the_post();
-	get_template_part( 'template-parts/content/content-page' );
-
-	// If comments are open or there is at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	}
-endwhile; // End of the loop.
-
-get_footer();
+    <?php echo get_the_content(); ?>
+<?php get_footer(); ?>
